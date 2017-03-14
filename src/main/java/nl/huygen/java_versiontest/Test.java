12fa022e5356e6e71c0f792b071f43a58034ae81
Paul Huygen <paul.huygen@huygen.nl>
2017-03-14 14:44:40 +0100

    initial version 0.0.0

---
+++ src/main/java/nl/huygen/java_versiontest/Test.java
@@ -0,0 +1,12 @@
+package nl.huygen.java_versiontest;
+
+
+public class Test
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World in version" + Versiontest.get_version());
+    }
+
+    
+}
---
+++ src/main/java/nl/huygen/java_versiontest/Versiontest.java
@@ -0,0 +1,12 @@
+package nl.huygen.java_versiontest;
+
+public class Versiontest {
+
+    static String version = "0.0.0";
+
+    public static String get_version() {
+	return version;
+    } 
+  
+}
+
