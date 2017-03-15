e2a0a71f57b5af233ad5b2dc89c47310602b35fe
Paul Huygen <paul.huygen@huygen.nl>
2017-03-15 11:20:14 +0100

    Up to version V0.0.1

--- src/main/java/nl/huygen/java_versiontest/Versiontest.java
+++ src/main/java/nl/huygen/java_versiontest/Versiontest.java
@@ -2,7 +2,7 @@ package nl.huygen.java_versiontest;
 
 public class Versiontest {
 
-    static String version = "0.0.0";
+    static String version = "0.0.1";
 
     public static String get_version() {
 	return version;
